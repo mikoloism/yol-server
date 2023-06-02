2520fbf08faf2d95d956846be47e8232548cf39b
mikoloism <mikoloism@gmail.com>
2023-06-02 12:48:16 +0330

    chore: update global.d.ts to sync `process.env` and `.env` file

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,20 @@
 declare namespace NodeJS {
 	export interface ProcessEnv {
-		REDIS_PUBLIC_NETWORK_HOST: string;
-		REDIS_PUBLIC_NETWORK_PORT: string;
-		REDIS_PASSWORD: string;
-		REDIS_URL: string;
+		/* LIARA REDIS ENV VARIABLES */
+		readonly LIARA_REDIS_PUBLIC_NETWORK_HOST: string;
+		readonly LIARA_REDIS_PUBLIC_NETWORK_PORT: string;
+		readonly LIARA_REDIS_PASSWORD: string;
+		readonly LIARA_REDIS_URL: string;
+
+		/* UPSTASH REDIS EVN VARIABLES */
+		readonly UPSTASH_REDIS_REST_URL: string;
+		readonly UPSTASH_REDIS_REST_TOKEN: string;
+		readonly REDIS_DATABASE_NAME: string;
+		readonly REDIS_DATABASE_ID: string;
+		readonly REDIS_REGION: string;
+		readonly REDIS_ENDPOINT: string;
+		readonly REDIS_PASSWORD: string;
+		readonly REDIS_PORT: string;
+		readonly REDIS_URL: string;
 	}
 }

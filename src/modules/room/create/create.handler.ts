7ff42e09faa115852fce2fab6cccc6f2bc50f4d7
mikoloism <mikoloism@gmail.com>
2023-06-04 15:57:20 +0330

    perf(m/room): refactor create handler

--- src/modules/room/create/create.handler.ts
+++ src/modules/room/create/create.handler.ts
@@ -1,8 +1,12 @@
 import { HttpStatusCode } from 'axios';
 import { redisClient } from 'libs/app/redis';
+import { Visibility } from '../room.enum';
 import { RoomCreateException } from '../room.exception';
 import type { CreatePostRequest, CreatePostResponse } from './create.interface';
 import { RoomValidator } from './create.validation';
+
+const ROOM_CREATE_DEFAULT_NAME = '';
+const ROOM_CREATE_DEFAULT_VISIBILITY = Visibility.PUBLIC;
 
 export async function room_create_new(
 	req: CreatePostRequest,
@@ -10,24 +14,23 @@ export async function room_create_new(
 ) {
 	try {
 		await RoomValidator.validateCreateReqBody(req.body);
-		const room_name = req.body.room_name ?? '';
-		const visibility = req.body.visibility ?? 'public';
+		const room_name = req.body.room_name ?? ROOM_CREATE_DEFAULT_NAME;
+		const visibility =
+			req.body.visibility ?? ROOM_CREATE_DEFAULT_VISIBILITY;
 		const created_at = new Date().toString();
 		await RoomValidator.validateRoomName(room_name);
-		const roomData = { room_name, created_at, visibility };
-
-		const redisMessage = await redisClient.set(
-			'room:'.concat(room_name),
-			JSON.stringify(roomData),
-		);
+		const roomKey = 'room:'.concat(room_name);
+		const rawData = { room_name, created_at, visibility };
+		const roomData = JSON.stringify(rawData);
+		const redisMessage = await redisClient.set(roomKey, roomData);
 		const isRedisStored = redisMessage === 'OK';
 
 		if (isRedisStored === false) {
 			throw new RoomCreateException('cannot store data in database');
 		}
 
-		const ROOM_CREATE_SUCCESS_RESPONSE = roomData;
-		const ROOM_CREATE_SUCESS_STATUS = HttpStatusCode.Ok;
+		const ROOM_CREATE_SUCCESS_RESPONSE = rawData;
+		const ROOM_CREATE_SUCESS_STATUS = HttpStatusCode.Created;
 		res.status(ROOM_CREATE_SUCESS_STATUS).send(
 			ROOM_CREATE_SUCCESS_RESPONSE,
 		);
